d840fa41681e3880f2850fede33616778196c098
Ray Hedgecock <raymondhedgecock@gmail.com>
2019-02-25 12:57:03 -0700

    tutorial 1 updates

--- README.md
+++ README.md
@@ -21,7 +21,7 @@ https://www.rabbitmq.com/download.html
 
 ###### Tutorial 1
 
-https://www.rabbitmq.com/tutorials/tutorial-one-java.html
+https://www.rabbitmq.com/tutorials/tutorial-one-spring-amqp.html
 
 To run the sender:
 ```
@@ -34,3 +34,22 @@ To run the receiver:
 java -jar build/libs/rabbitmq-amqp-tutorials-1.0.0.jar --spring.profiles.active=hello-world,receiver
 ```
 
+###### Tutorial 2
+
+https://www.rabbitmq.com/tutorials/tutorial-two-spring-amqp.html
+
+###### Tutorial 3
+
+https://www.rabbitmq.com/tutorials/tutorial-three-spring-amqp.html
+
+###### Tutorial 4
+
+https://www.rabbitmq.com/tutorials/tutorial-four-spring-amqp.html
+
+###### Tutorial 5
+
+https://www.rabbitmq.com/tutorials/tutorial-five-spring-amqp.html
+
+###### Tutorial 6
+
+https://www.rabbitmq.com/tutorials/tutorial-six-spring-amqp.html
--- src/main/java/org/springframework/amqp/tutorials/tut1/Tut1Config.java
+++ src/main/java/org/springframework/amqp/tutorials/tut1/Tut1Config.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2015-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.amqp.tutorials.tut1;
 
 import org.springframework.amqp.core.Queue;
@@ -5,7 +20,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-@Profile({"tut1","hello-world"})
+/**
+ * @author Gary Russell
+ * @author Scott Deeg
+ * @author Wayne Lund
+ */
+@Profile({"tut1", "hello-world"})
 @Configuration
 public class Tut1Config {
 
@@ -25,4 +45,5 @@ public class Tut1Config {
     public Tut1Sender sender() {
         return new Tut1Sender();
     }
+
 }
