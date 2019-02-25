c7e05da36f3b4e9d679d203c789f0ad74454a1d2
Ray Hedgecock <raymondhedgecock@gmail.com>
2019-02-25 12:38:27 -0700

    adding tutorials

--- src/main/java/org/springframework/amqp/tutorials/tut1/Tut1Config.java
+++ src/main/java/org/springframework/amqp/tutorials/tut1/Tut1Config.java
@@ -1,11 +1,11 @@
-package org.rabbitmq.tutorials.tut1;
+package org.springframework.amqp.tutorials.tut1;
 
 import org.springframework.amqp.core.Queue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-@Profile({"tut1", "hello-world"})
+@Profile({"tut1","hello-world"})
 @Configuration
 public class Tut1Config {
 
